ae88f6b28987a22ff3f68cd0535a95fc518daf59
alexandre <alexandre@debian>
2022-02-28 17:42:08 -0500

    suppr tout

--- include/lib/my_lib.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MYLIB__
-#define __MYLIB__
-
-void my_putchar(char);
-void my_putstr(char*);
-int my_strlen(char*);
-
-#endif
--- include/src/my_putchar.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-#include "../lib/my_lib.h"
-
-void my_putchar(char c)
-{
-  write(1, &c, 1);
-}
